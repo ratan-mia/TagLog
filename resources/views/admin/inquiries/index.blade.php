2688314525ef377cccf2a28f86003110eb8d75fb
Shorifull Islam <shorifull@gmail.com>
2020-02-03 09:01:38 +0600

    Update index.blade.php

--- resources/views/admin/inquiries/index.blade.php
+++ resources/views/admin/inquiries/index.blade.php
@@ -64,11 +64,12 @@
                                 {{ $inquiry->user->name ?? '' }}
                             </td>
                             <td>
-                                {{ $inquiry->agent->name ?? '' }}
-                            </td>
-                            <td>
                                 {{ $inquiry->email ?? '' }}
                             </td>
+                            <td>
+                                {{ $inquiry->agent->name ?? '' }}
+                            </td>
+
                             <td>
                                 {{ $inquiry->name ?? '' }}
                             </td>
@@ -83,22 +84,27 @@
                             </td>
                             <td>
                                 @can('inquiry_show')
-                                    <a class="btn btn-xs btn-primary" href="{{ route('admin.inquiries.show', $inquiry->id) }}">
+                                    <a class="btn btn-xs btn-primary"
+                                       href="{{ route('admin.inquiries.show', $inquiry->id) }}">
                                         {{ trans('global.view') }}
                                     </a>
                                 @endcan
 
                                 @can('inquiry_edit')
-                                    <a class="btn btn-xs btn-info" href="{{ route('admin.inquiries.edit', $inquiry->id) }}">
+                                    <a class="btn btn-xs btn-info"
+                                       href="{{ route('admin.inquiries.edit', $inquiry->id) }}">
                                         {{ trans('global.edit') }}
                                     </a>
                                 @endcan
 
                                 @can('inquiry_delete')
-                                    <form action="{{ route('admin.inquiries.destroy', $inquiry->id) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
+                                    <form action="{{ route('admin.inquiries.destroy', $inquiry->id) }}" method="POST"
+                                          onsubmit="return confirm('{{ trans('global.areYouSure') }}');"
+                                          style="display: inline-block;">
                                         <input type="hidden" name="_method" value="DELETE">
                                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                        <input type="submit" class="btn btn-xs btn-danger" value="{{ trans('global.delete') }}">
+                                        <input type="submit" class="btn btn-xs btn-danger"
+                                               value="{{ trans('global.delete') }}">
                                     </form>
                                 @endcan
 
@@ -127,7 +133,7 @@
                 url: "{{ route('admin.inquiries.massDestroy') }}",
                 className: 'btn-danger',
                 action: function (e, dt, node, config) {
-                    var ids = $.map(dt.rows({ selected: true }).nodes(), function (entry) {
+                    var ids = $.map(dt.rows({selected: true}).nodes(), function (entry) {
                         return $(entry).data('entry-id')
                     });
 
@@ -142,8 +148,11 @@
                             headers: {'x-csrf-token': _token},
                             method: 'POST',
                             url: config.url,
-                            data: { ids: ids, _method: 'DELETE' }})
-                            .done(function () { location.reload() })
+                            data: {ids: ids, _method: 'DELETE'}
+                        })
+                            .done(function () {
+                                location.reload()
+                            })
                     }
                 }
             }
@@ -151,11 +160,11 @@
             @endcan
 
             $.extend(true, $.fn.dataTable.defaults, {
-                order: [[ 1, 'desc' ]],
+                order: [[1, 'desc']],
                 pageLength: 50,
             });
-            $('.datatable-Inquiry:not(.ajaxTable)').DataTable({ buttons: dtButtons })
-            $('a[data-toggle="tab"]').on('shown.bs.tab', function(e){
+            $('.datatable-Inquiry:not(.ajaxTable)').DataTable({buttons: dtButtons})
+            $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
                 $($.fn.dataTable.tables(true)).DataTable()
                     .columns.adjust();
             });
